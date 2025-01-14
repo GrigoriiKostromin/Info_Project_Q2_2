491454a43472a258e75f06c6ee87e8f34c2262aa
GrigoriiKostromin <grigorii.kostromin@gmail.com>
2025-01-14 20:13:23 +0100

    First Commit - Player movement

---
+++ app/README.md
@@ -0,0 +1,16 @@
+# Videospiel
+
+
+### Instalation
+
+Um diese Anwendung zu starten müssen folgende Kriterien erfüllt werden:
+
+- Es sollte midestens Python 3.7 installiert sein.
+- Es sollte die neuste Version von TCOD installiert sein.
+
+Verwende `Python -V`, um die installierte Version zu überprüfen.
+Wenn die Pyhton Version unter 3.7 ist benutze ``
+Benutze `python install tcod`, um TCOD zu installieren.
+
+Um die Anwendung zu benutzen aktiviere das Terminal, bewege dich in den Ordner /app, starte main.py mit `python main.py`
+
---
+++ app/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import tcod
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+
+def main() -> None:
+
+    # Bildschirmgröße
+    screen_width = 80
+    screen_height = 50
+
+    my_files = ['accounts.txt', 'details.csv', 'invite.docx']
+   
+    # Spieler erscheint in der Mitte
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    # Wähle Zeichen aus Tileset
+    tileset = tcod.tileset.load_tilesheet(
+        "../dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    # Bildschirmcreation mit Console
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Roguelike",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+
+        # Spielschleife
+        while True:
+
+            # Spieler wird ausgeben
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            # Vorheriger Schritt wird gelöscht
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+                
+                # Funktion mit Input wird aufgerufen
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                # Spiel schließen ohne Fehlermeldung
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
